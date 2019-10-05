01b88d16640bfd78550b90518090f9d7ce8fcedb
Jillian Guillen <jguillen@spartaglobal.com>
2019-10-05 17:21:27 +0100

    connected gamesDb server to python

--- games_class.py
+++ games_class.py
@@ -1,13 +1,32 @@
 from connect_db import *
 # child-class inheriting from connect_db (Parent class)
 
-class Games_listings(ConnectionMicrosoftServer):
+class GamesListings(ConnectionMicrosoftServer):
 
-# Create a games listing
-# Read a listing
+    # Create a games listing
+    def add_a_listing(self, game_id, name, price, location, longitude, latitude):
+        query_rows = self.filter_query(
+            f"INSERT INTO game_listings VALUES ({game_id}, {name},{price}, {location}, {longitude}, {latitude})")
+        self.conn_gamesdb.commit()
 
-# As a user I can create a game listing
-# As a user I can get all games listing
-# As a user I can read one game listing
+    # Read a listing
+    def read_one_game_listing(self, table, game_id):
+        query = self.filter_query(f"SELECT * FROM {table} WHERE recipe_id = {game_id}")
+        while True:
+            record = query.fetchone()
+            if record is None:
+                break
+            return record
+
+    def list_all_games(self, table):
+        query = self.filter_query(f"SELECT * FROM {table}")
+        while True:
+            record = query.fetchall()
+            if record is None:
+                break
+            return record
+
+
+
 # As a user I want to insert a location to my listing (e.g. 125 London Wall) and I want it on my Db
 # As a user I want to be able to see a longitude and latitude to my listing and be on my Db
--- run_games.py
+++ run_games.py
@@ -1,8 +1,17 @@
 from connect_db import *
+from games_class import *
 
 server = 'localhost,1433'
 database = 'my_game'
 username = 'SA'
 password = 'Passw0rd2018'
 
-conn_gamesdb = (server, database, username, password)
+conn_gamesdb = GamesListings(server, database, username, password)
+
+# add a listing
+conn_gamesdb.add_a_listing(3, "'Spider-Man'", 23.85, "'HA5 5NZ'", "'-0.379288'", "'51.587946'")
+
+# list all listings
+#print(conn_gamesdb.list_all_games("game_listings"))
+
+
