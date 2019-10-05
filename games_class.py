b2867970b9275ab20d09d53872a74d6bde2a3e19
Jillian Guillen <jguillen@spartaglobal.com>
2019-10-05 15:49:21 +0100

    did pseudo code

--- games_class.py
+++ games_class.py
@@ -5,3 +5,9 @@ class Games_listings(ConnectionMicrosoftServer):
 
 # Create a games listing
 # Read a listing
+
+# As a user I can create a game listing
+# As a user I can get all games listing
+# As a user I can read one game listing
+# As a user I want to insert a location to my listing (e.g. 125 London Wall) and I want it on my Db
+# As a user I want to be able to see a longitude and latitude to my listing and be on my Db
