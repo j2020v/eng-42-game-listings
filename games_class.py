30d3aa87777ef536c9b730e9925f91e4666d418f
Jillian Guillen <jguillen@spartaglobal.com>
2019-10-05 22:41:45 +0100

    list all, read one, added location, longitude and latitude

--- games_class.py
+++ games_class.py
@@ -1,12 +1,14 @@
 from connect_db import *
+import requests
+import json
+
 # child-class inheriting from connect_db (Parent class)
 
 class GamesListings(ConnectionMicrosoftServer):
 
     # Create a games listing
     def add_a_listing(self, game_id, name, price, location, longitude, latitude):
-        query_rows = self.filter_query(
-            f"INSERT INTO game_listings VALUES ({game_id}, {name},{price}, {location}, {longitude}, {latitude})")
+        query_rows = self.filter_query(f"INSERT INTO game_listings VALUES ({game_id}, {name},{price}, {location}, {longitude}, {latitude}")
         self.conn_gamesdb.commit()
 
     # Read a listing
@@ -18,6 +20,7 @@ class GamesListings(ConnectionMicrosoftServer):
                 break
             return record
 
+    # Listing all games
     def list_all_games(self, table):
         query = self.filter_query(f"SELECT * FROM {table}")
         while True:
@@ -26,7 +29,11 @@ class GamesListings(ConnectionMicrosoftServer):
                 break
             return record
 
-
+    # Postcode info
+    def find_postcode_info(self, postcode):
+        postcode_query = requests.get('http://api.postcodes.io/postcodes/' + postcode)
+        result = postcode_query.json()
+        return result
 
 # As a user I want to insert a location to my listing (e.g. 125 London Wall) and I want it on my Db
 # As a user I want to be able to see a longitude and latitude to my listing and be on my Db
--- run_games.py
+++ run_games.py
@@ -8,10 +8,30 @@ password = 'Passw0rd2018'
 
 conn_gamesdb = GamesListings(server, database, username, password)
 
+# finding a postcode
+postcode_info = conn_gamesdb.find_postcode_info('EC2Y 5AS')
+postcode1 = (postcode_info['result']['postcode'])
+
+# finding long and lat for postcode1
+long1 = int(postcode_info['result']['longitude'])
+lat1 = int(postcode_info['result']['latitude'])
+
 # add a listing
-conn_gamesdb.add_a_listing(3, "'Spider-Man'", 23.85, "'HA5 5NZ'", "'-0.379288'", "'51.587946'")
+conn_gamesdb.add_a_listing(4, "'Mark of the Ninja'", 6.90 + postcode1 + long1 + lat1)
+
+#postcode2info = conn_gamesdb.find_postcode_info('E2 8LU')
+#postcode3info = conn_gamesdb.find_postcode_info('E9 6RY')
+
+# finding long and lat for postcode2
+#long2 = (postcode2['result']['longitude'])
+#lat2 = (postcode2['result']['latitude'])
+
+# finding long and lat for postcode3
+#long3 = (postcode3['result']['longitude'])
+#lat3 = (postcode3['result']['latitude'])
+
+
 
 # list all listings
 #print(conn_gamesdb.list_all_games("game_listings"))
-
 
