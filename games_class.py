1dee3962589c6689dc07598a52865bca9aa47fe3
Jillian Guillen <jguillen@spartaglobal.com>
2019-10-07 14:58:59 +0100

    completed interface, thank you Sam

--- games_class.py
+++ games_class.py
@@ -7,13 +7,14 @@ import json
 class GamesListings(ConnectionMicrosoftServer):
 
     # Create a games listing
-    def add_a_listing(self, game_id, name, price, location, longitude, latitude):
-        query_rows = self.filter_query(f"INSERT INTO game_listings VALUES ({game_id}, {name},{price}, {location}, {longitude}, {latitude}")
+    def add_a_listing(self, game_id, name, price, location, longitude='', latitude=''):
+        query = (f"INSERT INTO game_listings VALUES ({game_id}, '{name}', {price}, '{location}', '{longitude}', '{latitude}')")
+        self.filter_query(query)
         self.conn_gamesdb.commit()
 
     # Read a listing
-    def read_one_game_listing(self, table, game_id):
-        query = self.filter_query(f"SELECT * FROM {table} WHERE game_id = {game_id}")
+    def read_one_game_listing(self, game_id):
+        query = self.filter_query(f"SELECT * FROM game_listings WHERE game_id = {game_id}")
         while True:
             record = query.fetchone()
             if record is None:
@@ -30,16 +31,27 @@ class GamesListings(ConnectionMicrosoftServer):
             return record
 
     # Postcode info
-    def find_postcode_info(self, postcode):
-        postcode_query = requests.get('http://api.postcodes.io/postcodes/' + postcode)
-        result = postcode_query.json()
-        return result
+    def find_long_info(self, postcode):
+        postcode_query = requests.get(f'http://api.postcodes.io/postcodes/'+ postcode)
+        result_postcode_query = postcode_query.json()
+        long = result_postcode_query['result']['longitude']
+        self.conn_gamesdb.commit()
+        return float(long)
+
+    def find_lat_info(self, postcode):
+        postcode_query = requests.get(f'http://api.postcodes.io/postcodes/' + postcode)
+        result_postcode_query = postcode_query.json()
+        lat = result_postcode_query['result']['latitude']
+        self.conn_gamesdb.commit()
+        return float(lat)
 
     # Update location, long, lat
     def update_game_listing(self, table, column, value, game_id):
-        query_rows = self.filter_query(f"UPDATE {table} SET {column} = {value} WHERE game_id = {game_id}")
+        query = (f"UPDATE {table} SET {column} = {value} WHERE game_id = {game_id}")
+        self.filter_query(query)
         self.conn_gamesdb.commit()
 
 
 # As a user I want to insert a location to my listing (e.g. 125 London Wall) and I want it on my Db
 # As a user I want to be able to see a longitude and latitude to my listing and be on my Db
+
--- run_games.py
+++ run_games.py
@@ -8,39 +8,67 @@ password = 'Passw0rd2018'
 
 conn_gamesdb = GamesListings(server, database, username, password)
 
-# finding info postcode1
-postcode_info = conn_gamesdb.find_postcode_info('E2 8LU')
-postcode1 = postcode_info['result']['postcode']
-# finding long and lat for postcode1
-long1 = float(postcode_info['result']['longitude'])
-lat1 = float(postcode_info['result']['latitude'])
-
-# finding info postcode2
-postcode_info = conn_gamesdb.find_postcode_info('EC2Y 5AS')
-postcode2 = postcode_info['result']['postcode']
-# finding long and lat for postcode2
-long2 = float(postcode_info['result']['longitude'])
-lat2 = float(postcode_info['result']['latitude'])
-
-
-#update location for postcode1
-conn_gamesdb.update_game_listing('game_listings', 'location', "'E2 8LU'", 2)
-conn_gamesdb.update_game_listing('game_listings', 'longitude', long1, 2)
-conn_gamesdb.update_game_listing('game_listings', 'latitude', lat1, 2)
-
-#update location for postcode2
-conn_gamesdb.update_game_listing('game_listings', 'location', "'EC2Y 5AS'", 3)
-conn_gamesdb.update_game_listing('game_listings', 'longitude', long2, 3)
-conn_gamesdb.update_game_listing('game_listings', 'latitude', lat2, 3)
+# # finding info postcode1
+# #postcode_info = conn_gamesdb.find_postcode_info('E2 8LU')
+# #postcode1 = postcode_info['result']['postcode']
+# # finding long and lat for postcode1
+# long1 = float(postcode_info['result']['longitude'])
+# lat1 = float(postcode_info['result']['latitude'])
+#
+# # finding info postcode2
+# #postcode_info = conn_gamesdb.find_postcode_info('EC2Y 5AS')
+# postcode2 = postcode_info['result']['postcode']
+# # finding long and lat for postcode2
+# long2 = float(postcode_info['result']['longitude'])
+# lat2 = float(postcode_info['result']['latitude'])
+#
+#
+# #update location for postcode1
+# conn_gamesdb.update_game_listing('game_listings', 'location', "'E2 8LU'", 2)
+# conn_gamesdb.update_game_listing('game_listings', 'longitude', long1, 2)
+# conn_gamesdb.update_game_listing('game_listings', 'latitude', lat1, 2)
+#
+# #update location for postcode2
+# conn_gamesdb.update_game_listing('game_listings', 'location', "'EC2Y 5AS'", 3)
+# conn_gamesdb.update_game_listing('game_listings', 'longitude', long2, 3)
+# conn_gamesdb.update_game_listing('game_listings', 'latitude', lat2, 3)
 
 # add a listing
 #conn_gamesdb.add_a_listing(3, "'NBA 2K19'", 13.49, "'EC2Y 5AS'", long2, lat2)
-
-
-
 
 # list all listings
 #print(conn_gamesdb.list_all_games("game_listings"))
 
 # read one listing
 #print(conn_gamesdb.read_one_game_listing("game_listings", 2))
+
+# conn_gamesdb.lat_info(postcode_info)
+
+greeting = print("Hello, you have entered Game Tree. What would you like to do?")
+print('(1) View all listings')
+print('(2) View one listing')
+print('(3) Add a game listing')
+user_input = int(input("Enter your choice"))
+while True:
+    if user_input == 1:
+        print(conn_gamesdb.list_all_games("game_listings"))
+        user_input = int(input("Enter a choice again:"))
+    elif user_input == 2:
+        user_input = int(input("Enter a game ID:"))
+        print(conn_gamesdb.read_one_game_listing(user_input))
+        user_input = int(input("Enter a choice again:"))
+    elif user_input == 3:
+        print("You've chosen to add a game to our listing.")
+        game_id = int(input("Assign your game an ID:"))
+        game_name = input('Enter the name of the game:').capitalize().strip()
+        game_price = (float(input('How much would you like to sell it for?')))
+        game_location = input('Enter a location (postcode):')
+        game_long_info = conn_gamesdb.find_long_info(game_location)
+        game_lat_info = conn_gamesdb.find_lat_info(game_location)
+        print(f'{game_name} has now been added to the listing!')
+        conn_gamesdb.add_a_listing(game_id, game_name, game_price, game_location, game_long_info, game_lat_info)
+
+        break
+    else:
+        print("I am not sure what you mean...")
+        break
